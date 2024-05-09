d8cf0ed09a0fa3e89eaf877d272eeaf22eed2c80
Anup-Banstola <anupin09@gmail.com>
2024-05-09 09:28:19 +0545

    navigae to dashboard if logged in

--- src/config/firebase-config.jsx
+++ src/config/firebase-config.jsx
@@ -9,8 +9,6 @@ import {
   signInWithPopup,
 } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { createBrowserRouter, useNavigate } from "react-router-dom";
-import firebase from "firebase/compat/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -25,23 +23,19 @@ const firebaseConfig = {
   appId: "1:216593805064:web:72dd79a9cc2e3d879b86bd",
   measurementId: "G-06B2Z8R0CY",
 };
-
+signInWithPopup;
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
-
 setPersistence(auth, browserLocalPersistence)
   .then(() => {
     const user = auth.currentUser;
     console.log(user);
-
     if (user) {
-      const navigate = useNavigate();
       console.log("User is already signed in:", user);
-      navigate("/dashboard");
     } else {
       signInWithPopup(auth, provider);
     }
@@ -51,8 +45,3 @@ setPersistence(auth, browserLocalPersistence)
     const errorMessage = error.message;
     console.log("Error setting persistence:", errorMessage);
   });
-
-const browserHistory = createBrowserRouter();
-firebase.auth().onAuthStateChanged((user) => {
-  browserHistory.replace(user ? "/dashboard" : "/");
-});
--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -2,12 +2,27 @@ import { auth, provider } from "../../config/firebase-config";
 import { signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import styles from "./index.module.css";
+import { useEffect, useState } from "react";
+import Loader from "../../components/reports/daily/Loader";
 
 export const Auth = () => {
+  const [loading, setloading] = useState(true);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setloading(false);
+      if (user) {
+        navigate("/dashboard");
+      }
+    });
+    return () => unsubscribe();
+  }, [navigate]);
+
   const signInWithGoogle = async () => {
     const results = await signInWithPopup(auth, provider);
     console.log(results);
+
     const authInfo = {
       userID: results.user.uid,
       name: results.user.displayName,
@@ -19,6 +34,10 @@ export const Auth = () => {
 
     navigate("/dashboard");
   };
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <div className={styles.signin}>
       <p className={styles.sign}>Sign In With Google to Continue</p>
