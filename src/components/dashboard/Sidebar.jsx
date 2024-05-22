51f8b86c7a43c8804787344bfdc5bee971c90f95
Anup-Banstola <anupin09@gmail.com>
2024-05-22 16:49:57 +0545

    sidebaredit

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -69,42 +69,52 @@ function Sidebar() {
   return (
     <>
       <div className={styles.sidebar}>
-        <NavLink to="/dashboard" className={styles.headlink}>
-          <div className={styles.logoContainer}>
-            <img
-              src="/assets/icons/logo.svg"
-              alt="Logo"
-              className={styles.logo}
-              title="Expense tracker"
-            />
-            {!isSmallScreen && (
-              <h2 className={styles.title}>Expense Tracker</h2>
+        <div className={styles.sidesignout}>
+          <div>
+            {" "}
+            <NavLink to="/dashboard" className={styles.headlink}>
+              <div className={styles.logoContainer}>
+                <img
+                  src="/assets/icons/logo.svg"
+                  alt="Logo"
+                  className={styles.logo}
+                  title="Expense tracker"
+                />
+                {!isSmallScreen && (
+                  <h2 className={styles.title}>Expense Tracker</h2>
+                )}
+              </div>
+            </NavLink>
+            <div className={styles.sidelinks}>
+              {sidebarLinks.map((link, index) => (
+                <div className={styles.nav} key={index}>
+                  <NavLink
+                    to={link.link}
+                    className={styles.link}
+                    title={link.title}
+                  >
+                    <FontAwesomeIcon icon={link.icon} className={styles.icon} />
+                    <span className={styles.text}>{link.title}</span>
+                  </NavLink>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className={styles.signoutContainer}>
+            {!isSmallScreen ? (
+              <span className={styles.signout} onClick={signUserOut}>
+                Sign Out
+              </span>
+            ) : (
+              <FontAwesomeIcon
+                icon={faSignOut}
+                className={styles.logoutbtn}
+                title="Sign Out"
+                onClick={signUserOut}
+              />
             )}
           </div>
-        </NavLink>
-
-        {sidebarLinks.map((link, index) => (
-          <div className={styles.nav} key={index}>
-            <NavLink to={link.link} className={styles.link} title={link.title}>
-              <FontAwesomeIcon icon={link.icon} className={styles.icon} />
-              <span className={styles.text}>{link.title}</span>
-            </NavLink>
-          </div>
-        ))}
-
-        <div className={styles.signoutContainer}>
-          {!isSmallScreen ? (
-            <span className={styles.signout} onClick={signUserOut}>
-              Sign Out
-            </span>
-          ) : (
-            <FontAwesomeIcon
-              icon={faSignOut}
-              className={styles.logoutbtn}
-              title="Sign Out"
-              onClick={signUserOut}
-            />
-          )}
         </div>
       </div>
     </>
